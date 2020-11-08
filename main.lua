bc52a351eb09e9d17175a2cf05456fb714060357
99heitor <99heitor@gmail.com>
2020-11-08 13:40:29 -0300

    Implement racket movement as physics simulation
    This allows the racket to transfer energy to the ball

--- main.lua
+++ main.lua
@@ -5,16 +5,16 @@ function love.load()
     playerHeight = 50
 
     player1 = {}
-    player1.body = love.physics.newBody(world, 200, 400)
+    player1.body = love.physics.newBody(world, 200, 400, "kinematic")
     player1.shape = love.physics.newRectangleShape(0, 0, playerWidth, playerHeight)
-    player1.fixture = love.physics.newFixture(player1.body, player1.shape, 5)
+    player1.fixture = love.physics.newFixture(player1.body, player1.shape, 2)
     player1.body:setAngle(math.rad(135))
     player1.playerArea = {beginning = 0, ending = 400}
 
     player2 = {}
-    player2.body = love.physics.newBody(world, 550, 400)
+    player2.body = love.physics.newBody(world, 550, 400, "kinematic")
     player2.shape = love.physics.newRectangleShape(0, 0, playerWidth, playerHeight)
-    player2.fixture = love.physics.newFixture(player2.body, player2.shape, 5)
+    player2.fixture = love.physics.newFixture(player2.body, player2.shape, 2)
     player2.body:setAngle(math.rad(45))
     player2.playerArea = {beginning = 400, ending = 800}
 
@@ -22,7 +22,7 @@ function love.load()
     ball = {}
     ball.body = love.physics.newBody(world, 650/2, 650/2, "dynamic")
     ball.shape = love.physics.newCircleShape(5)
-    ball.fixture = love.physics.newFixture(ball.body, ball.shape, 0.001)
+    ball.fixture = love.physics.newFixture(ball.body, ball.shape, 1)
     ball.fixture:setRestitution(1)
     ball.body:setLinearVelocity(300, 0)
        
@@ -53,63 +53,63 @@ function validatePlayerMovement(player, nextMoveX, nextMoveY)
     return false
 end
 
+function validateDeadzone(axis1, axis2)
+    return (math.abs(axis1) >= 0.03 and math.abs(axis2) >= 0.03)
+end
+
 function movePlayer1()
-    local nextMoveX = player1.body:getX()
-    local nextMoveY = player1.body:getY()
+    local linearVelocityX = 0
+    local linearVelocityY = 0
     if love.keyboard.isDown("a") then
-        nextMoveX = nextMoveX-10
+        linearVelocityX = -1000
     end
 
     if love.keyboard.isDown("s") then
-        nextMoveY = nextMoveY+10
+        linearVelocityY = 1000
     end
 
     if love.keyboard.isDown("d") then
-        nextMoveX = nextMoveX+10
+        linearVelocityX = 1000
     end
 
     if love.keyboard.isDown("w") then
-        nextMoveY = nextMoveY-10
+        linearVelocityY = -1000
     end
 
-    if joystick then
-        nextMoveX = nextMoveX + joystick:getAxis(1)*10
-        nextMoveY = nextMoveY + joystick:getAxis(2)*10
-    end
-
-    if validatePlayerMovement(player1, nextMoveX, nextMoveY) then
-        player1.body:setX(nextMoveX)
-        player1.body:setY(nextMoveY)
+    if love.keyboard.isDown("a", "s", "d", "w") then
+        player1.body:setLinearVelocity(linearVelocityX, linearVelocityY)
+    elseif joystick and validateDeadzone(joystick:getAxis(1), joystick:getAxis(2)) then
+        player1.body:setLinearVelocity(joystick:getAxis(1)*1000, joystick:getAxis(2)*1000)
+    else 
+        player1.body:setLinearVelocity(0, 0)
     end
 end
 
 function movePlayer2()
-    local nextMoveX = player2.body:getX()
-    local nextMoveY = player2.body:getY()
+    local linearVelocityX = 0
+    local linearVelocityY = 0
     if love.keyboard.isDown("left") then
-        nextMoveX = nextMoveX-10
+        linearVelocityX = -1000
     end
 
     if love.keyboard.isDown("down") then
-        nextMoveY = nextMoveY+10
+        linearVelocityY = 1000
     end
 
     if love.keyboard.isDown("right") then
-        nextMoveX = nextMoveX+10
+        linearVelocityX = 1000
     end
 
     if love.keyboard.isDown("up") then
-        nextMoveY = nextMoveY-10
+        linearVelocityY = -1000
     end
 
-    if joystick then
-        nextMoveX = nextMoveX + joystick:getAxis(3)*10
-        nextMoveY = nextMoveY + joystick:getAxis(4)*10
-    end
-
-    if validatePlayerMovement(player2, nextMoveX, nextMoveY) then
-        player2.body:setX(nextMoveX)
-        player2.body:setY(nextMoveY)
+    if love.keyboard.isDown("left", "down", "right", "up") then
+        player2.body:setLinearVelocity(linearVelocityX, linearVelocityY)
+    elseif joystick and validateDeadzone(joystick:getAxis(3), joystick:getAxis(4)) then
+        player2.body:setLinearVelocity(joystick:getAxis(3)*1000, joystick:getAxis(4)*1000)
+    else 
+        player2.body:setLinearVelocity(0, 0)
     end
 end
 
