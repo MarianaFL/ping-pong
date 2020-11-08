487cad7ed00cfddb5bd37d3a156a9677a261bffb
MarianaFL <mflgeb@hotmail.com>
2020-11-08 19:08:15 -0300

    Add colliding logs, reset ball position, add ball and net

--- main.lua
+++ main.lua
@@ -1,5 +1,12 @@
+--prints colliding fixtures info and collision position
+function beginContact(a, b, coll)
+    local x,y = coll:getPositions()
+    print(a:getUserData().." colliding with "..b:getUserData().." "..x..", "..y)
+end
+
 function love.load()
     world = love.physics.newWorld(0, 300, true)
+    world:setCallbacks(beginContact)
     
     playerWidth = 15
     playerHeight = 50
@@ -9,33 +16,47 @@ function love.load()
     gamepad1 = {xAxis = 1, yAxis = 2}
     gamepad2 = {xAxis = 3, yAxis = 4}
 
-
     player1 = {}
-    player1.body = love.physics.newBody(world, 200, 400, "kinematic")
+    player1.body = love.physics.newBody(world, 200, 400, "dynamic")
     player1.shape = love.physics.newRectangleShape(0, 0, playerWidth, playerHeight)
     player1.fixture = love.physics.newFixture(player1.body, player1.shape, 2)
-    player1.body:setAngle(math.rad(135))
+    -- player1.body:setAngle(math.rad(135))
+    player1.body:setFixedRotation(true)
+    player1.body:setGravityScale(0)
     player1.playerArea = {beginning = 0, ending = 400}
     player1.controls = {keyboard = {up = "w", left = "a", down = "s", right = "d"}, gamepad = gamepad1}
+    player1.fixture:setUserData('P1')
 
     player2 = {}
-    player2.body = love.physics.newBody(world, 550, 400, "kinematic")
+    player2.body = love.physics.newBody(world, 550, 400, "dynamic")
     player2.shape = love.physics.newRectangleShape(0, 0, playerWidth, playerHeight)
     player2.fixture = love.physics.newFixture(player2.body, player2.shape, 2)
-    player2.body:setAngle(math.rad(45))
+    -- player2.body:setAngle(math.rad(45))
+    player2.body:setFixedRotation(true)
+    player2.body:setGravityScale(0)
     player2.playerArea = {beginning = 400, ending = 800}
     player2.controls = {keyboard = {up = "up", left = "left", down = "down", right = "right"}, gamepad = gamepad2}
-
+    player2.fixture:setUserData('P2')
     
     ball = {}
     ball.body = love.physics.newBody(world, 650/2, 650/2, "dynamic")
     ball.shape = love.physics.newCircleShape(5)
     ball.fixture = love.physics.newFixture(ball.body, ball.shape, 1)
     ball.fixture:setRestitution(1)
-    ball.body:setLinearVelocity(300, 0)
+    ball.body:setGravityScale(0)
+    ball.fixture:setUserData('ball')
        
+    gameTable = {}
+    gameTable.body = love.physics.newBody(world, 400, 450)
+    gameTable.shape = love.physics.newRectangleShape(0, 0, 500, 20)
+    gameTable.fixture = love.physics.newFixture(gameTable.body, gameTable.shape, 5)
+    gameTable.fixture:setUserData('table')
 
-
+    net = {}
+    net.body = love.physics.newBody(world, 400, 415)
+    net.shape = love.physics.newRectangleShape(0, 0, 20, 70)
+    net.fixture = love.physics.newFixture(net.body, net.shape, 5)
+    net.fixture:setUserData('net')
 
 end
 
@@ -46,6 +67,10 @@ function love.draw()
 
     love.graphics.setColor(0, 1, 0)
     love.graphics.circle("fill", ball.body:getX(), ball.body:getY(), ball.shape:getRadius())
+
+    love.graphics.setColor(1, 1, 1)
+    love.graphics.polygon("fill", gameTable.body:getWorldPoints(gameTable.shape:getPoints()))
+    love.graphics.polygon("fill", net.body:getWorldPoints(net.shape:getPoints()))
     
 end
 
@@ -71,19 +96,19 @@ function movePlayer(player)
     playerkb = player.controls.keyboard
 
     if love.keyboard.isDown(playerkb.left) then
-        linearVelocityX = -1000
+        linearVelocityX = -200
     end
 
     if love.keyboard.isDown(playerkb.down) then
-        linearVelocityY = 1000
+        linearVelocityY = 200
     end
 
     if love.keyboard.isDown(playerkb.right) then
-        linearVelocityX = 1000
+        linearVelocityX = 200
     end
 
     if love.keyboard.isDown(playerkb.up) then
-        linearVelocityY = -1000
+        linearVelocityY = -200
     end
 
     playerGmpd = player.controls.gamepad
@@ -105,4 +130,18 @@ function love.update(dt)
     end
     movePlayer(player1)
     movePlayer(player2)
+
+    --if ball go off the screen, reset it's initial status
+    local x, y = ball.body:getPosition()
+    if x < 0 or x > 800 or y < 0 or y > 600 then
+        ball.body:setPosition(650/2, 650/2)
+        ball.body:setLinearVelocity(0, 0)
+        ball.body:setGravityScale(0)
+    end
+
+    --if ball is moving, guarantee it's following gravity
+    local vx, vy = ball.body:getLinearVelocity()
+    if vx ~= 0 or vy ~= 0 then
+        ball.body:setGravityScale(1)
+    end
 end
